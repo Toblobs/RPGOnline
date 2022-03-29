edb72a05c4dd2d1f344fcab477a7caf9e5532d6a
Tobiloba Kujore <91623811+Toblobs@users.noreply.github.com>
2022-03-29 13:44:49 +0100

    v1.0.1 - Framework
    Nothing but the frame of the project, empty shells to be filled. Two paths - handling and connection.

---
+++ main.py
@@ -0,0 +1,13 @@
+### RPGOnline
+### A Synergy Studios Project
+
+version = '1.0.1'
+patch = '#a0001'
+
+from lib.handling.entities import *
+from lib.handling.io import *
+
+from lib.connection.socket_server import *
+from lib.connection.packet import *
+
+
