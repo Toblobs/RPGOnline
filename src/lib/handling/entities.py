d863c9a2ca4b8b256491d05d6c7e491456cf233c
Tobiloba Kujore <91623811+Toblobs@users.noreply.github.com>
2022-03-29 19:26:58 +0100

    v1.0.1-a002 (entities.py)
    Adds a hero class, soon to have skillsets, types and more.

---
+++ src/lib/handling/entities.py
@@ -0,0 +1,17 @@
+### RPGOnline
+### A Synergy Studios Project
+
+class Entity:
+
+    """A class for every type of thing."""
+
+    def __init__(self):
+        pass
+
+
+class Hero(Entity):
+
+    """A hero which is represented a character which has a skillset
+       and is controlled by a player."""
+
+    pass
