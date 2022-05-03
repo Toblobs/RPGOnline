7df6ddfb0758b304f80cbaa1675f3763756e65ff
Tobiloba Kujore <91623811+Toblobs@users.noreply.github.com>
2022-05-03 13:16:21 +0100

    v1.0.1-a003 (main.py)
    Nothing new, just update of the patch variable and labelling of imports

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@
 ### A Synergy Studios Project
 
 version = '1.0.1'
-patch = '#a0002'
+patch = '#a003'
 
+# Handling
 from lib.handling.entities import *
 from lib.handling.io import *
 
+# Connection 
 from lib.connection.socket_server import *
 from lib.connection.packet import *
-
-
